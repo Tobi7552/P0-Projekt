3852d16ede8d6f3b1a0b3122f4012f4b49927d8d
TheEngbjerg <112902320+TheEngbjerg@users.noreply.github.com>
2022-09-19 08:59:30 +0200

    Add files via upload

--- P0_Projekt_V3/main.py
+++ P0_Projekt_V3/main.py
@@ -14,45 +14,87 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
 ev3 = EV3Brick()
-
 #Motor
-right_wheel = Motor(Port.B)
-left_wheel = Motor(Port.A)
+right_wheel = Motor(Port.D)
+left_wheel = Motor(Port.C)
 robot = DriveBase(left_wheel, right_wheel, wheel_diameter = 55, axle_track = 157)
-klo_motor = Motor(Port.C)
-
+klo_motor = Motor(Port.B)
+klo_sensor = UltrasonicSensor(Port.S1)
 
 #Input
-color = ColorSensor(Port.S1)
+color = ColorSensor(Port.S2)
 touch = TouchSensor(Port.S4)
-
-
-#Farver
-grey = color.reflection() + 2 
-sort = grey / 3
-hvid = 85
-treshold = (grey + hvid)/2
-
-rettelse = 3.5
 
 
 # Write your program here.
 ev3.speaker.beep(50,1000)
-#robot.straight(500)
+
+def linje(speed,a):                            
+    if 65 <= color.reflection() <= 75 or color.reflection() <= 20:
+        robot.drive(speed,0)
+    elif color.reflection() > 75:
+        robot.drive(speed, (a*-0.2)*(color.reflection()-75)**2)
+    elif color.reflection() < 65:
+        robot.drive(speed, (a*0.2)*(color.reflection()-75)**2)
+
+level = 0 
+
+def sort(speed):
+    if 5 < color.reflection() < 10:
+        print(level+1)
+        robot.drive(speed,0)
+        ev3.speaker.beep(500,500)
+        return 1
+    return 0
 
 
-def linje():
-    while True:
-        deviation = color.reflection() - treshold
-        turn_rate = rettelse * deviation
-        turn_rate2 = rettelse * -deviation
-        robot.drive(100, turn_rate)
-        robot.drive(100, turn_rate2)
-        if touch.pressed():
-            klo_motor.run_target(500,-400, wait = True)
-            klo_motor.run_target(500,400)
-            
+def level2():
+    robot.turn(45)
+    robot.straight(180)
+    robot.turn(-45)
+    linje(90,1)
 
-linje()
+def level3():
+    robot.turn(-45)
+    robot.straight(180)
+    robot.turn(45)
+    linje(90,1)
 
-        
+def level4():
+    robot.turn(35)
+    while klo_sensor.distance() >= 65:
+        linje(35,1)
+    else:
+        robot.drive(0,0)
+        ev3.speaker.beep(500,500)
+        klo_motor.run_until_stalled(-500, then=Stop.BRAKE,duty_limit=40)
+        robot.drive(35,0)
+
+
+
+def level5():
+    robot.drive(0,0)
+    klo_motor.run_until_stalled(500, then=Stop.BRAKE,duty_limit=40)
+    robot.straight(-50)
+    robot.turn(-90)
+    linje(75,1)
+
+list_ = [['level2()', 0],['level3()', 0],['level4()', 0],['level5()', 0]]
+
+
+while True:
+    print(klo_sensor.distance())
+    if not touch.pressed():
+        level += sort(75)
+
+        if list_[level-1][1] == 0 and level != 0:
+            eval(list_[level-1][0])
+            list_[level-1][1] = 1
+
+
+        if level != 3:
+            linje(90,1)
+
+
+    else:
+        robot.stop()
