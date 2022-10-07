88912dd982103139da520bfe63a4160301d4ee06
TheEngbjerg <112902320+TheEngbjerg@users.noreply.github.com>
2022-10-07 09:53:07 +0200

    Add files via upload

--- P0_Projekt_V3/main.py
+++ P0_Projekt_V3/main.py
@@ -27,7 +27,6 @@ touch = TouchSensor(Port.S4)
 
 
 # Write your program here.
-ev3.speaker.beep(50,1000)
 
 def linje(speed,a):                            
     if 65 <= color.reflection() <= 75 or color.reflection() <= 20:
@@ -41,9 +40,8 @@ level = 0
 
 def sort(speed):
     if 5 < color.reflection() < 10:
+        robot.straight(20)
         print(level+1)
-        robot.drive(speed,0)
-        ev3.speaker.beep(500,500)
         return 1
     return 0
 
@@ -65,33 +63,39 @@ def level3():
         linje(70,1)
     else:
         robot.drive(0,0)
-        ev3.speaker.beep(500,500)
-        klo_motor.run_until_stalled(1000, then=Stop.BRAKE,duty_limit=100)
+        klo_motor.run_until_stalled(1000, then=Stop.HOLD,duty_limit=60)
         robot.drive(50,0)
 
 
 
 def level4():
     robot.drive(0,0)
-    klo_motor.run_until_stalled(-1000, then=Stop.BRAKE,duty_limit=40)
+    klo_motor.run_until_stalled(-1000, then=Stop.HOLD,duty_limit=40)
     robot.straight(-50)
     robot.turn(-90)
 
 
 def level5():
-    robot.straight(57)
+    robot.straight(80)
     robot.turn(-90)
 
 def level6(x):
     robot.turn(10)
-    robot.straight(110)
+    robot.straight(120)
     robot.turn(-20)
     robot.straight(110)
-    robot.turn(10)
-    while color.reflection() >= 55:
+    while color.reflection() >= 50:
         linje(90,1)
     robot.straight(400)
-    robot.turn(x*90)
+    robot.turn(x*80)
+    if x == 1:
+        robot.straight(50)
+        robot.reset()
+        while robot.distance() < 2400:
+            linje(90,1) 
+        while color.reflection() > 10:
+            linje(45,1)
+
 
 def level7():
     robot.reset()
@@ -106,39 +110,34 @@ def level7():
     robot.turn(45)
 
 def level8():
-    robot.straight(60)
+    robot.straight(80)
     robot.turn(-90)
     
 
 def level9():
-    robot.straight(300)
+    robot.straight(120)
+    robot.turn(80)
     robot.reset()
-    while klo_sensor.distance() > 575:
-        robot.turn(-1)
-    robot.turn(-10)
-    while klo_sensor.distance() >= 33 and robot.distance() < 600:
-        robot.drive(35,0)
+    while robot.distance() < 955:
+        linje(90,-1)
+    robot.turn(70)
+    robot.straight(50)
+    robot.reset()
+    while klo_sensor.distance() > 33 and robot.distance() < 30:
+        robot.drive(10,0)
     robot.drive(0,0)
-    klo_motor.run_until_stalled(1000, then=Stop.BRAKE,duty_limit=100)
-    robot.straight(-robot.distance()-50)
-    robot.drive(0,0)
-    robot.turn(-robot.angle()-10)
-    klo_motor.run_until_stalled(-1000, then=Stop.BRAKE,duty_limit=40)
-    robot.straight(-50)
-    robot.turn(-90)
-    robot.straight(150)
-    while color.reflection() > 65:
-        robot.drive(90,1)
-    robot.turn(-90)
-    while color.reflection() > 10:
-        linje(50,1)
+    klo_motor.run_until_stalled(1000, then=Stop.HOLD,duty_limit=63)
+    robot.reset()
+    
+    robot.straight(-430)
+    klo_motor.run_until_stalled(-1000, then=Stop.HOLD,duty_limit=40)
+    robot.straight(-260)
+    robot.turn(-110)
+    while color.reflection() > 60:
+        robot.drive(100,0)
+    robot.straight(50)
+    robot.turn(-60)
 
-def level10():
-    robot.reset()
-    while robot.distance() < 500:
-        linje(90,1)
-    robot.straight(100)
-    robot.turn(-90)
 
 
 def level11():
@@ -155,32 +154,39 @@ def level12():
     while robot.distance() < 40:
         linje(35,1)
     robot.turn(45)
-    robot.straight(40)
+    robot.straight(50)
     robot.turn(-45)
-    while klo_sensor.distance() > 100:
+    while klo_sensor.distance() > 230:
         robot.drive(70,0)
-    d = robot.distance()
-    robot.turn(-45)
-    while klo_sensor.distance() > 100:
-        robot.drive(70,3)
-    robot.turn(70)
-    robot.straight(250)
-    robot.turn(-robot.angle())
-    robot.straight(d-100)
+    while klo_sensor.distance() < 350:
+        robot.drive(0,-10)
+    robot.turn(-5)
+    while klo_sensor.distance() > 300:
+        robot.drive(70,0)
+    while klo_sensor.distance() < 1000:
+        robot.drive(70,24)
+    robot.turn(15)
+    robot.straight(120)
+    while color.reflection() > 65:
+        robot.drive(70,-12)
 
+        
 def level13():
+    robot.straight(100)
+    robot.turn(60)
+    robot.straight(60)
+    robot.turn(-60)
     robot.reset()
-    while robot.distance() < 20:
-        linje(10,0.1)
-    while color.reflection() > 40:
-        robot.drive(150,0)
-    robot.straight(-(robot.distance()/2))
+    while robot.distance() < 1150:
+        linje(90,-1)
+    robot.turn(-70)
+    robot.straight(110)
     while True:
-        robot.drive(0,0)
+        robot.stop()
 
 
-list_ = [['level1()', 0],['level2()', 0],['level3()', 0],['level4()', 0],['level5()', 0],['level6(1)', 0],['level6(-1)', 0],['level7()', 0],['level8()', 0],['level9()',0],['level10()', 0],['level11()', 0],['level12()', 0],['level11()', 0],['level13()', 0]]
-
+list_ = [['level1()', 0],['level2()', 0],['level3()', 0],['level4()', 0],['level5()', 0],['level6(1)', 0],['level6(-1)', 0],['level7()', 0],['level8()', 0],['level9()', 0],['level11()', 0],['level12()', 0],['level11()', 0],['level13()', 0]]
+klo_motor.run_until_stalled(-1000, then=Stop.BRAKE,duty_limit=40)
 while True:
     if not touch.pressed():
         level += sort(75)
